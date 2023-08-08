ecb65453a928ce412cc913d91189acf6d0693a91
Cristian Trenado <cristiantrenado41@gmail.com>
2023-08-08 19:07:17 +0200

    diseño responsive movil

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <div className="rounded-sm border-2 border-black flex flex-row mt-12 mr-12 ml-12 bg-[#13242b] gradient-background">
-        <div className="w-3/6 flex-column">
+        <div className="w-1/3 flex-column">
           <div className="font-medium h-1/3">
             <p id="mainText" className="xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm pl-4 pt-2 text-white msize">
               BARHAIN
@@ -19,39 +19,40 @@ export default function Home() {
             <p className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm pl-4 text-white msize" id="subtitle">
               BEST LAP TIME
             </p>
-            <p id="number" className="text-white pl-11 mt-4 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
+            <p id="lapNumber" className="text-white pl-11 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
               1:31.447
+            </p>
+            <p className="text-white pl-11 mt-4 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
+              
             </p>
           </div>
         </div>
 
         <div className="block place-items: center">
-        <div className="h-1/2 flex items-center justify-center">
-            <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 border-2  border-white text-xs text-white	">
-              326
-            </p>
+        <div className="h-1/2 flex flex-col items-center justify-center">
+        <div id="smallCircle" className="flex items-center justify-center xl:w-14 xl:h-14 lg:w-12 lg:h-12 md:w-10 md:h-10 sm:w-10 sm:h-8 border-3 border-white rounded-full">
+              <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 text-xs text-white 	">3</p>
+            </div>
+            <p id="hiddenText" className="text-white text-center justify-center sm:hidden">Max. Speed</p>
           </div>
-          
-          
-          <div className="h-1/2 ">
-            <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 border-2  border-white text-xs text-white ">
-              250
-            </p>
+
+          <div  className=" flex flex-col items-center justify-center ">
+          <div id="smallCircle" className="flex items-center justify-center w-14 h-14 border-3 border-white rounded-full">
+              <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 text-xs text-white 	">3</p>
+            </div>
+            <p id="hiddenText" className="text-white text-center justify-center ">DRS Zone</p>
           </div>
 
         </div>
           
-        
-
-        <div className="font-medium w-24 ml-auto w-3/6">
-          <img
-            className="w-full h-full object-cover"
+        <div className="font-medium ml-auto w-8/12">
+          <img  className="w-full h-full object-cover"
             src="https://media.formula1.com/image/upload/content/dam/fom-website/2018-redesign-assets/Circuit%20maps%2016x9/Bahrain_Circuit.png"
           />
         </div>
       </div>
       <div className="rounded-sm border-2 border-black flex flex-row mt-12 mr-12 ml-12 bg-[#13242b] gradient-background">
-        <div className="w-3/6 flex-column">
+        <div className="w-1/3 flex-column">
           <div className="font-medium h-1/3">
             <p id="mainText" className="xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm pl-4 pt-2 text-white msize">
             JEDDAH
@@ -61,75 +62,77 @@ export default function Home() {
             <p className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm pl-4 text-white msize" id="subtitle">
               BEST LAP TIME
             </p>
-            <p id="number" className="text-white pl-11 mt-4 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
+            <p id="lapNumber" className="text-white pl-11 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
             1.27.500
+            </p>
+            <p className="text-white pl-11 mt-4 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
+              
             </p>
           </div>
         </div>
 
         <div className="block place-items: center">
-        <div className="h-1/2 flex items-center justify-center">
-            <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 border-2  border-white text-xs text-white	">
-              326
-            </p>
+        <div className="h-1/2 flex flex-col items-center justify-center">
+        <div id="smallCircle" className="flex items-center justify-center xl:w-14 xl:h-14 lg:w-12 lg:h-12 md:w-10 md:h-10 sm:w-10 sm:h-8 border-3 border-white rounded-full">
+              <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 text-xs text-white 	">3</p>
+            </div>
+            <p id="hiddenText" className="text-white text-center justify-center sm:hidden">Max. Speed</p>
           </div>
-          
-          
-          <div className="h-1/2 ">
-            <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 border-2  border-white text-xs text-white ">
-              250
-            </p>
+
+          <div  className=" flex flex-col items-center justify-center ">
+          <div id="smallCircle" className="flex items-center justify-center w-14 h-14 border-3 border-white rounded-full">
+              <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 text-xs text-white 	">3</p>
+            </div>
+            <p id="hiddenText" className="text-white text-center justify-center ">DRS Zone</p>
           </div>
 
         </div>
           
-        
-
-        <div className="font-medium w-24 ml-auto w-3/6">
-          <img
-            className="w-full h-full object-cover"
-            src="https://media.formula1.com/image/upload/content/dam/fom-website/2018-redesign-assets/Circuit%20maps%2016x9/Bahrain_Circuit.png"
+        <div className="font-medium ml-auto w-8/12">
+          <img  className="w-full h-full object-cover"
+            src="https://media.formula1.com/image/upload/content/dam/fom-website/2018-redesign-assets/Circuit%20maps%2016x9/Saudi_Arabia_Circuit.png.transform/9col/image.png"
           />
         </div>
       </div>
       <div className="rounded-sm border-2 border-black flex flex-row mt-12 mr-12 ml-12 bg-[#13242b] gradient-background">
-        <div className="w-3/6 flex-column">
+        <div className="w-1/3 flex-column">
           <div className="font-medium h-1/3">
             <p id="mainText" className="xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm pl-4 pt-2 text-white msize">
-              ALBERT PARK
+            ALBERT PARK
             </p>
           </div>
           <div className="font-medium h-1/3">
             <p className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm pl-4 text-white msize" id="subtitle">
               BEST LAP TIME
             </p>
-            <p id="number" className="text-white pl-11 mt-4 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
+            <p id="lapNumber" className="text-white pl-11 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
             1:16.700
+            </p>
+            <p className="text-white pl-11 mt-4 xl:text-4xl lg:text-2xl md:text-2xl sm:text-sm">
+              
             </p>
           </div>
         </div>
 
         <div className="block place-items: center">
-        <div className="h-1/2 flex items-center justify-center">
-            <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 border-2  border-white text-xs text-white	">
-              326
-            </p>
+        <div className="h-1/2 flex flex-col items-center justify-center">
+        <div id="smallCircle" className="flex items-center justify-center xl:w-14 xl:h-14 lg:w-12 lg:h-12 md:w-10 md:h-10 sm:w-10 sm:h-8 border-3 border-white rounded-full">
+              <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 text-xs text-white 	">3</p>
+            </div>
+            <p id="hiddenText" className="text-white text-center justify-center sm:hidden">Max. Speed</p>
           </div>
-          
-          
-          <div className="h-1/2 ">
-            <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 border-2  border-white text-xs text-white ">
-              250
-            </p>
+
+          <div  className=" flex flex-col items-center justify-center ">
+          <div id="smallCircle" className="flex items-center justify-center w-14 h-14 border-3 border-white rounded-full">
+              <p id="number" className="xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm block rounded-full px-2 py-2 text-xs text-white 	">3</p>
+            </div>
+            <p id="hiddenText" className="text-white text-center justify-center ">DRS Zone</p>
           </div>
 
         </div>
           
-        
-
-        <div className="font-medium w-24 ml-auto w-3/6">
-          <img
-            className="w-full h-full object-cover"
+        <div className="font-medium ml-auto w-8/12">
+          <img  className="w-full h-full object-cover"
             src="https://media.formula1.com/image/upload/content/dam/fom-website/2018-redesign-assets/Circuit%20maps%2016x9/Australia_Circuit.png"
           />
         </div>
